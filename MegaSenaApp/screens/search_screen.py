a74ab51c5490c265a869bfeb0b9999fa6f7c6500
Eddie <eddiemeira@gmail.com>
2026-02-18 10:54:13 -0500

    Created a PS file for the title images and rearranged text in the search page

--- MegaSenaApp/screens/search_screen.py
+++ MegaSenaApp/screens/search_screen.py
@@ -2,4 +2,7 @@ from kivy.uix.screenmanager import Screen
 
 
 class SearchScreen(Screen):
-    pass
+    
+    def text_display_search(self):
+        self.ids.result_label.text = "Números pesquisados se baseiam \n " \
+        "no período de Nov/1996 a Fev/2026"
